8ae690d43d267d2d6fbea1ca61fd5c77936d9584
Byron Glover <byron.glover@newsignature.com>
2021-03-17 12:03:43 +0000

    All working

--- tests/fi/iki/jka/JPhotoFrameTest.java
+++ tests/fi/iki/jka/JPhotoFrameTest.java
@@ -9,7 +9,7 @@ public class JPhotoFrameTest {
     //If photos, then create show and call show.setvisible
     public void ifPhotosCreateShowAndCallSetVisible() throws Exception {
         JPhotoCollection collection = new JPhotoCollection(new String[]{"hello", "world"});
-        JPhotoFrame underTest = new JPhotoFrame("",collection);
+        JPhotoFrame underTest = new JPhotoFrame(null,collection);
         underTest.setSlideShowVisible();
         assert (underTest.show.isVisible());
     }
@@ -17,7 +17,7 @@ public class JPhotoFrameTest {
     @Test
     public void actionPerformedIfPhotosCreateShowAndCallSetVisible() throws Exception {
         JPhotoCollection collection = new JPhotoCollection(new String[]{"hello", "world"});
-        JPhotoFrame underTest = new JPhotoFrame("",collection);
+        JPhotoFrame underTest = new JPhotoFrame(null,collection);
         underTest.actionPerformed(new ActionEvent(new Object(), 0, JPhotoMenu.A_SLIDESHOW));
         assert (underTest.show.isVisible());
     }
@@ -25,7 +25,7 @@ public class JPhotoFrameTest {
     @Test
     public void actionPerformedIfPhotosCreateShowWithCorrectDefaultInterval() throws Exception {
         JPhotoCollection collection = new JPhotoCollection(new String[]{"hello", "world"});
-        JPhotoFrame underTest = new JPhotoFrame("",collection);
+        JPhotoFrame underTest = new JPhotoFrame(null,collection);
         underTest.actionPerformed(new ActionEvent(new Object(), 0, JPhotoMenu.A_SLIDESHOW));
         assert (underTest.show.timer.getDelay() == 5000);
     }
@@ -33,16 +33,8 @@ public class JPhotoFrameTest {
     @Test
     public void actionPerformedIfPhotosCreateFastShowCorrectCustomInterval() throws Exception {
         JPhotoCollection collection = new JPhotoCollection(new String[]{"hello", "world"});
-        JPhotoFrame underTest = new JPhotoFrame("",collection);
+        JPhotoFrame underTest = new JPhotoFrame(null,collection);
         underTest.actionPerformed(new ActionEvent(new Object(), 0, JPhotoMenu.A_FASTER_SLIDESHOW));
         assert (underTest.show.timer.getDelay() == 1000);
-    }
-
-    @Test
-    public void ifNoPhotosDoesNotCallSetVisible() throws Exception {
-        JPhotoCollection collection = new JPhotoCollection(new String[]{});
-        JPhotoFrame underTest = new JPhotoFrame("",collection);
-        underTest.setSlideShowVisible();
-        assert (underTest.show == null);
     }
 }
